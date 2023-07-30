6c83f6cbdfa3ae1ae8c298b85e46b41741c1c99b
Joshua Sigston <joshua.sigston@outlook.com>
2023-07-30 13:46:19 -0600

    final draft commit

--- mobile_nav.js
+++ mobile_nav.js
@@ -1,6 +1,24 @@
-const hamburger = document.getElementById('hamburger')
-const mobileNav = document.getElementById('mobile-nav')
+const hamburger = document.getElementById('hamburger');
+const mobileNav = document.getElementById('mobile-nav');
+const navLink = document.querySelectorAll('.nav_link');
+const patty = document.querySelectorAll('.patty');
+
+function stateChange() {
+    hamburger.classList.toggle('hamburger_change');
+    
+    patty.forEach( item => {
+        item.classList.toggle('patty_change')
+    })
+};
 
 hamburger.addEventListener('pointerdown', () => {
-    mobileNav.classList.toggle('nav_toggle')
-})
+    mobileNav.classList.toggle('nav_toggle');
+    stateChange();
+});
+
+navLink.forEach( link => {
+    link.addEventListener('pointerdown', () => {
+        mobileNav.classList.toggle('nav_toggle');
+        stateChange();
+    });
+});
